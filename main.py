ef2608eb7cc16c661cf65259addd58b74800fe76
leo-exee <leopold@littlebill.io>
2024-10-24 13:16:13 +0200

    feat(init): init genome count with chuck params

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi import status
 
 from models.http_response import ErrorResponse
 from models.item import ItemOutDTO
@@ -17,18 +18,54 @@ REDUCE = "$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.ja
     response_model=list[ItemOutDTO],
     summary="Count the words in a text",
     description="Take a file, count the words and return the word count list",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Invalid file format",
+            "content": {
+                "application/json": {
+                    "example": {"status": 400, "message": "Invalid file format"}
+                }
+            },
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Internal server error",
+            "content": {
+                "application/json": {
+                    "example": {"status": 500, "message": "Internal server error"}
+                }
+            },
+        },
+    },
 )
 async def count_word_in_text_controller(file: UploadFile):
     return count_word_in_text_service(file)
 
 
 @app.post(
-    "/",
+    "/genome",
     response_model=list[ItemOutDTO],
-    summary="Count the words in a genom",
+    summary="Count the words in a genome",
     description="Take a file, count the words and return the word count list",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Invalid file format",
+            "content": {
+                "application/json": {
+                    "example": {"status": 400, "message": "Invalid file format"}
+                }
+            },
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Internal server error",
+            "content": {
+                "application/json": {
+                    "example": {"status": 500, "message": "Internal server error"}
+                }
+            },
+        },
+    },
 )
-async def count_word_in_genom_controller(file: UploadFile):
+async def count_word_in_genome_controller(file: UploadFile, chunk: int = 8):
     return count_word_in_text_service(file)
 
 
